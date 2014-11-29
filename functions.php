22911683e56d82c7ba69a0217985f1e4b2567e20
Seki Kagami <moe.pascal@gmail.com>
2014-11-30 04:42:06 +0800

    add enqueue scripts

--- functions.php
+++ functions.php
@@ -100,6 +100,14 @@ function tsuyuri_scripts() {
 
 	wp_enqueue_script( 'tsuyuri-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	// wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', array(), '1.11.1', true );
+
+	// wp_enqueue_script( 'jquery.sticky-kit', get_template_directory_uri() . '/js/jquery.sticky-kit.js', array(), '1.1.1', true );
+
+	wp_enqueue_script( 'headhesive', get_template_directory_uri() . '/js/headhesive.js', array(), '1.1.1', true );
+
+	wp_enqueue_script( 'tsuyuri', get_template_directory_uri() . '/js/tsuyuri.js', array(), 'dev', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
