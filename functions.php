acd525acf8bf8937a629e3c96ab5ede70dc77199
Seki Kagami <moe.pascal@gmail.com>
2014-12-01 23:54:20 +0800

    changed to enqueue fontawesome

--- functions.php
+++ functions.php
@@ -94,11 +94,11 @@ add_action( 'widgets_init', 'tsuyuri_widgets_init' );
 function tsuyuri_scripts() {
 	wp_enqueue_style( 'webfont', get_template_directory_uri() . '/webfont/webfont.css', array(), '1.0' );
 
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.3' );
+	wp_enqueue_style( 'tsuyuricons', get_template_directory_uri() . '/tsuyuricons/tsuyuricons.css', array(), '1.1' );
 
-	wp_enqueue_style( 'tsuyuri-style', get_stylesheet_uri() ,array( 'genericons','webfont' ));
+	wp_enqueue_style( 'tsuyuri-style', get_stylesheet_uri() ,array( 'tsuyuricons','webfont' ));
 
-	wp_enqueue_script( 'tsuyuri-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	// wp_enqueue_script( 'tsuyuri-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'tsuyuri-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
