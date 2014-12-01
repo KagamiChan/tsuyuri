8e3a8d7f671899b99b3eb745a63bc15674504e71
Seki Kagami <moe.pascal@gmail.com>
2014-12-01 22:12:43 +0800

    translate linode ref into CN

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'tsuyuri' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'tsuyuri' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'tsuyuri' ), 'tsuyuri', '<a href="http://kagami.moe" rel="designer">Seki Kagami</a>' ); ?>
-			<span class="sep"> | </span>Hosted at <a href="https://www.linode.com/?r=d4c7f3d8ab048130904fefe6d7dd68f80b861223" rel="host reference">Linode</a>
+			<span class="sep"> | </span>托管于 <a href="https://www.linode.com/?r=d4c7f3d8ab048130904fefe6d7dd68f80b861223" rel="host reference">Linode</a>
 			<br/>
 			2013-<script>document.write(new Date().getFullYear());</script> 鏡 ＠ がんばらないプロジェクト/翠星製作所
 		</div><!-- .site-info -->
